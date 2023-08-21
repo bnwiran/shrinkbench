6a174ad70c58a7698c543e1043a145d7ccf087ac
Belal Ahmad Nwiran <bnwiran@gmail.com>
2023-08-21 19:40:51 -0400

    Add new model
    Add ShuffleNetV2-based model.

--- models/tiny_models.py
+++ models/tiny_models.py
@@ -9,7 +9,7 @@ from torchmetrics.classification import MulticlassAccuracy
 from torchvision import models
 
 
-class MobileNetSmallV3Light(pl.LightningModule):
+class MobileNetSmallV3(pl.LightningModule):
     def __init__(self, num_classes: int, pretrained=False):
         super().__init__()
         assert num_classes > 1, "Number of classes must be greater than" + str(num_classes)
@@ -96,4 +96,75 @@ class MobileNetSmallV3Light(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+class ShuffleNetV2(pl.LightningModule):
+    def __init__(self, num_classes: int, pretrained=False):
+        super().__init__()
+        assert num_classes > 1, "Number of classes must be greater than" + str(num_classes)
+        model = models.shufflenet_v2_x0_5(weights=models.ShuffleNet_V2_X0_5_Weights.DEFAULT)
+
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features, num_classes)
+        model.fc.is_classifier = True
+
+        self.model = model
+        self._create_metrics(num_classes)
+
+    def _create_metrics(self, num_classes):
+        self.train_acc1 = MulticlassAccuracy(num_classes=num_classes)
+        self.train_acc5 = MulticlassAccuracy(num_classes=num_classes, top_k=5)
+        self.val_acc1 = MulticlassAccuracy(num_classes=num_classes)
+        self.val_acc5 = MulticlassAccuracy(num_classes=num_classes, top_k=5)
+        self.test_acc1 = MulticlassAccuracy(num_classes=num_classes)
+        self.test_acc5 = MulticlassAccuracy(num_classes=num_classes, top_k=5)
+
+    def training_step(self, batch, batch_idx) -> Tensor:
+        x, y = batch
+        logits = self(x)
+
+        loss = nn.functional.cross_entropy(logits, y)
+
+        self.train_acc1(logits, y)
+        self.train_acc5(logits, y)
+        self.log('train_acc1', self.train_acc1, prog_bar=True, on_step=False, on_epoch=True)
+        self.log('train_acc5', self.train_acc5, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("train_loss", loss, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+
+        loss = F.cross_entropy(logits, y)
+
+        self.val_acc1(logits, y)
+        self.val_acc5(logits, y)
+        self.log('val_acc1', self.val_acc1, prog_bar=True, on_step=False, on_epoch=True)
+        self.log('val_acc5', self.val_acc5, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("val_loss", loss, prog_bar=True)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+
+        loss = F.cross_entropy(logits, y)
+
+        self.test_acc1(logits, y)
+        self.test_acc5(logits, y)
+        self.log('test_acc1', self.test_acc1, prog_bar=True, on_step=False, on_epoch=True)
+        self.log('test_acc5', self.test_acc5, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("test_loss", loss, prog_bar=True)
+
+        return loss
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
         return optimizer
