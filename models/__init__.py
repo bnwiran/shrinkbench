e4e6db479e56039c544d25ed4af5fa82e5f4defd
Belal Ahmad Nwiran <bnwiran@gmail.com>
2023-08-20 21:37:44 -0400

    Fix __init__ file

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,10 @@
-import os
-import pathlib
-
-
-from .head import replace_head
-from .mnistnet import MnistNet
-from .tiny_models import MobileNetSmallV3, MobileNetSmallV3Light
 from .cifar_resnet import (resnet20,
                            resnet32,
                            resnet44,
                            resnet56,
                            resnet110,
                            resnet1202)
-
 from .cifar_vgg import vgg_bn_drop, vgg_bn_drop_100
+from .head import replace_head
+from .mnistnet import MnistNet
+from .tiny_models import MobileNetSmallV3Light
